83a81db5e8acdb656b5b9846c68613f4f9b82d8d
phenix502 <huangcong502@gmail.com>
2013-09-01 20:00:05 +0800

    create a project

---
+++ baidu/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for baidu project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'baidu'
+
+SPIDER_MODULES = ['baidu.spiders']
+NEWSPIDER_MODULE = 'baidu.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'baidu (+http://www.yourdomain.com)'
